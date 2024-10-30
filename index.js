3416fd7997222e7fa1cd770455d8e5245d2b6561
Andreas Eriksson <ae225ak@student.lnu.se>
2024-10-30 21:33:16 +0100

    Add template in html

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+// Import and use the custom element aer-randint
+
 import './aer-randint/index.js'
 
 const aerRandintElement = document.createElement('aer-randint')
 
 document.body.appendChild(aerRandintElement)
+
+// Using a template from the html file
+
+const template = document.querySelector('#example-template')
+const templateClone = template.content.cloneNode(true)
+
+document.querySelector('body').appendChild(templateClone)
+templateClone.textContent = 'Hello from the template'
